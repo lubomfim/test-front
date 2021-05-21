2b3074d839e51653005d72a34765cdcb65b2aa46
Lucas Bomfim <lucasliko@hotmail.com>
2021-05-21 18:11:26 -0300

    fix: removed redirect

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import { ProductsProvider } from 'contexts/checkout'
 
 import Cart from 'pages/Cart'
@@ -14,9 +14,6 @@ const Routes = () => {
           <Route exact path="/checkout/cart" component={Cart} />
           <Route exact path="/checkout/payment" component={Payment} />
           <Route exact path="/checkout/paid" component={Paid} />
-          <Route exact path="/">
-            <Redirect to="/checkout/cart" />
-          </Route>
         </ProductsProvider>
       </Switch>
     </Router>
